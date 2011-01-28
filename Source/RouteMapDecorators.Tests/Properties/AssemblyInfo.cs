f2ffa23acab7a76c0970402aabc50b7030c9184c
c29m <c29.hunter@gmail.com>
2011-01-28 22:28:01 +0100

    Created VS solution.

---
+++ Source/RouteMapDecorators.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RouteMapDecorators.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rmacfie")]
+[assembly: AssemblyProduct("RouteMapDecorators.Tests")]
+[assembly: AssemblyCopyright("Copyright © Rmacfie 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8d0b5a54-119e-4eb1-8fd6-773f33c27e7a")]
+[assembly: AssemblyVersion("0.9.*")]
---
+++ Source/RouteMapDecorators/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RouteMapDecorators")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rmacfie")]
+[assembly: AssemblyProduct("RouteMapDecorators")]
+[assembly: AssemblyCopyright("Copyright © Rmacfie 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("644a038b-6b54-4802-a071-c4eaf55c6a29")]
+[assembly: AssemblyVersion("0.9.*")]
